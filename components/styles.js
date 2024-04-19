36477b500bc79e7286e48195215e7e67d4b797b5
fihra <fabrofabian@gmail.com>
2024-04-18 22:53:57 -1000

    separated styles to another file

---
+++ components/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    tempoStyle: {
+      fontWeight: 'bold',
+      color: 'blue'
+    },
+    tempoForm: {
+        width: 100,
+        fontSize: 40
+    },
+    tempoBold: {
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    modSpacing: {
+        paddingTop: 12,
+        paddingBottom: 12
+    },
+    modTempoFont: {
+        fontSize: 30
+    },
+    pointMain: {
+        color: 'blue'
+    },
+    point25: {
+        color: 'orange'
+    },
+    point5: {
+        color: 'green'
+    },
+    point75: {
+        color: 'red'
+    }
+  });
