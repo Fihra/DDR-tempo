84cc1a383c76ca1971f73bd5e8da4e8a84acf294
fihra <fabrofabian@gmail.com>
2024-04-18 20:28:10 -1000

    adjust tempoForm file name

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import TempoForm from './components/TempoForm';
+import TempoForm from './components/tempoForm';
 
 export default function App() {
   return (
